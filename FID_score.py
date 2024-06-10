13c56143bd83462f6399c8720d22e17bd616b2a9
panyu <942088566@qq.com>
2024-06-10 17:58:51 +0800

    Init

--- FID_score.py
+++ FID_score.py
@@ -2,18 +2,22 @@ from datasets import load_from_disk
 from diffusers import StableDiffusionControlNetPipeline, ControlNetModel
 import torch
 from pytorch_fid.fid_score import calculate_fid_given_paths
-
+from utils import *
+from config.config import Config
+from transformers import DPTForDepthEstimation, DPTFeatureExtractor
 
 dataset = load_from_disk("models/cat_depth2dog_dataset")
 test_text = []
 test_condition = []
+depth_estimation = DPTForDepthEstimation.from_pretrained("./models/depth_estimation_model_save", torch_dtype=torch.float32).to("cuda")
+depth_extractor = DPTFeatureExtractor.from_pretrained("./models/depth_feature_extractor_model_save", torch_dtype=torch.float32).to("cuda")
 for i in range(0, len(dataset)):
     item = dataset[i]
     prompt = item['text']
     image = item['image']
-    test_condition = item['depthmap']
-    image.save("output/original_images/"+str(i)+".png")
-    test_text.append(prompt)
+    image = paste_image(image, Config)
+    test_condition = extract_depth(image, Config, depth_estimation, depth_extractor)
+    test_text.append(prompt.replace("cat", "kitty"))
     test_condition.append(test_condition)
 
 pipeline = StableDiffusionControlNetPipeline.from_pretrained("./models/diffusion_model_save", torch_dtype=torch.float32, safety_checker=None).to("cuda")
